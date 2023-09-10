75329fbb86c10321eb19b38ab06b3e41cb395695
Yonas <yonasseyoum24@gmail.com>
2023-09-10 16:13:22 +0300

    add components and styles

---
+++ application/src/components/job.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+
+
+export default function Job() {
+    return (
+        <div>
+            <div>Our Latest Jobs</div>
+            <div>Explore one of our latest and highly rewarding jobs from top companis
+                around the world</div>
+            <div></div>
+            <div>Load More Jobs</div>
+        </div>
+    )
+}
