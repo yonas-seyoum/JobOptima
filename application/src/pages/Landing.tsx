cf09075a66d61901a6a9c0f0fb49f86c0b7563ae
Yonas <yonasseyoum24@gmail.com>
2023-09-12 19:27:33 +0300

    add components to the landing page

--- application/src/pages/Landing.tsx
+++ application/src/pages/Landing.tsx
@@ -1,21 +1,11 @@
 import * as React from "react";
-import Nav from '../components/nav';
-import Hero from '../components/hero';
-import Job from '../components/job';
-import About from '../components/about';
-import Contact from '../components/contact';
-import Footer from '../components/footer';
-// import "../styles/style.css"
+import "../styles/landing.css"
+import Intro from "../components/intro/intro";
 
 export default function Landing() {
     return (
-        <div className="style">
-            <Nav />
-            <Hero />
-            <Job />
-            <About />
-            <Contact />
-            <Footer />
+        <div>
+            <Intro/>
         </div>
     )
 }
