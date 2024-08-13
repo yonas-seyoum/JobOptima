29281053f97bbf029ed82f6b0ed1594e63ff6306
Yonas <yonasseyoum24@gmail.com>
2024-08-13 15:43:00 +0300

    remove nav component

--- application/src/pages/Landing.tsx
+++ application/src/pages/Landing.tsx
@@ -2,15 +2,11 @@ import * as React from "react";
 import "../styles/landing.css"
 import Intro from "../components/intro/intro";
 import InfoSection from "../components/info-section/infoSection";
-import Nav from "../components/intro/nav";
-import JobsPreview from "../components/jobs/jobsPreview";
 
 export default function Landing() {
     return (
         <>
-            <Nav/>
             <Intro/>
-            <JobsPreview/>
             <InfoSection/>
         </>
     )
