a075a9a4823c3df4503424ef777f094df409268b
Yonas <yonasseyoum24@gmail.com>
2024-08-16 12:21:33 +0300

    add experience filter functionality

--- application/src/pages/jobs/components/experienceLevel.tsx
+++ application/src/pages/jobs/components/experienceLevel.tsx
@@ -7,13 +7,56 @@ import {
   FilterContent,
   FilterSection,
 } from "../styled-components/styledComponents";
+import { useJobListContext } from "../context/jobListContext.provider";
+
 
 export default function ExperienceLevel() {
   const [openSection, setOpenSection] = useState<boolean>(false);
+  const [experienceLevel, setExperienceLevel] = useState<string[]>([]);
+  const [isChecked, setIsChecked] = useState<{ [key: string]: boolean }>({
+    checkBox1: false,
+    checkBox2: false,
+    checkBox3: false,
+    checkBox4: false,
+  });
+
+  const { updateFilter } = useJobListContext();
 
   const handleSection = () => {
     setOpenSection(!openSection);
   };
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, checked } = event.target;
+
+    setIsChecked((prevState) => ({
+      ...prevState,
+      [name]: checked,
+    }));
+
+    if (checked) {
+      addExperienceLevel(event.target.value);
+    } else {
+      removeExperienceLevel(event.target.value);
+    }
+  };
+
+  const addExperienceLevel = (experience: string) => {
+    setExperienceLevel((prev) => {
+      const updatedLevels = [...prev, experience];
+      updateFilter(updatedLevels);
+
+      return updatedLevels;
+    });
+  };
+  const removeExperienceLevel = (experience: string) => {
+    setExperienceLevel((prev) => {
+      const updatedLevels = prev.filter((exp) => exp !== experience);
+      updateFilter(updatedLevels);
+      return updatedLevels;
+    });
+  };
+
   return (
     <FilterSection isOpen={true}>
       <DropdownHeader onClick={handleSection}>
@@ -23,19 +66,43 @@ export default function ExperienceLevel() {
       <FilterContent isOpen={openSection}>
         <CheckboxGroup>
           <CheckboxLabel>
-            <CheckboxInput type="checkbox" value="entry-level" />
+            <CheckboxInput
+              name="checkBox1"
+              checked={isChecked.checkBox1}
+              onChange={handleChange}
+              type="checkbox"
+              value="entry-level"
+            />
             Entry Level
           </CheckboxLabel>
           <CheckboxLabel>
-            <CheckboxInput type="checkbox" value="internship" />
+            <CheckboxInput
+              name="checkBox2"
+              checked={isChecked.checkBox2}
+              onChange={handleChange}
+              type="checkbox"
+              value="internship"
+            />
             Internship
           </CheckboxLabel>
           <CheckboxLabel>
-            <CheckboxInput type="checkbox" value="mid-level" />
+            <CheckboxInput
+              name="checkBox3"
+              checked={isChecked.checkBox3}
+              onChange={handleChange}
+              type="checkbox"
+              value="mid-level"
+            />
             Mid Level
           </CheckboxLabel>
           <CheckboxLabel>
-            <CheckboxInput type="checkbox" value="senior" />
+            <CheckboxInput
+              name="checkBox4"
+              checked={isChecked.checkBox4}
+              onChange={handleChange}
+              type="checkbox"
+              value="senior"
+            />
             Senior
           </CheckboxLabel>
         </CheckboxGroup>
