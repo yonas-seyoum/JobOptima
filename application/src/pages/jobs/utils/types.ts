65353e2c0db323807f50fd2d038745b426849356
Yonas <yonasseyoum24@gmail.com>
2024-08-19 22:59:15 +0300

    add industry filter functionality

--- application/src/pages/jobs/utils/types.ts
+++ application/src/pages/jobs/utils/types.ts
@@ -2,8 +2,8 @@ export type Job = {
   id: number;
   position: string;
   company: string;
-  jobType: string; 
-  workType: string; 
+  jobType: string;
+  workType: string;
   industry: string;
   experience: string;
   description: {
@@ -18,15 +18,19 @@ export type Job = {
   deadline: string;
 };
 
-export type Filter = ExperienceLevel;
+export type Filter = ExperienceLevel | Industry;
 export type ExperienceLevel = string[];
+export type Industry = string[];
 
 export type JobDetailContextType = {
   currentJob: Job;
   selectJob: (id: number) => any;
-}
+};
 
 export type JobListContextType = {
   jobList: Job[];
   updateFilter: (filter: Filter) => any;
+  updateExperienceFilter: (experience: ExperienceLevel) => any;
+  updateIndustryFilter: (industry: Industry) => any;
+  updateSalaryFilter: (salary: number) => any;
 };
