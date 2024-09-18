036c8afcd7ea55a2ec21fc88038106fe111bc42e
Yonas <yonasseyoum24@gmail.com>
2024-09-18 18:50:57 +0300

    refactor code

---
+++ application/src/pages/Landing/constants.tsx
@@ -0,0 +1,33 @@
+import { CheckCircle, Info, Build, ThumbUp, Star } from "@mui/icons-material";
+
+export const steps = [
+  {
+    icon: <Info />,
+    title: "Learn More",
+    description:
+      "Understand the features and benefits of our product or service.",
+  },
+  {
+    icon: <Build />,
+    title: "Get Started",
+    description:
+      "Follow the simple steps to start using our service or product.",
+  },
+  {
+    icon: <ThumbUp />,
+    title: "Enjoy",
+    description: "Experience the benefits and enjoy the results.",
+  },
+  {
+    icon: <CheckCircle />,
+    title: "Feedback",
+    description: "Share your thoughts and help us improve.",
+  },
+];
+
+export const descriptions = [
+  "Quick and easy setup.",
+  "User-friendly interface.",
+  "24/7 customer support.",
+  "Highly customizable options.",
+];
