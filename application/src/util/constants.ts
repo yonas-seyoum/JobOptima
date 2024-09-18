898af3f1f002a8d8402ee4bd298fa410ea1a05de
Yonas <yonasseyoum24@gmail.com>
2024-09-18 18:52:38 +0300

    create new files for constants and types

---
+++ application/src/util/constants.ts
@@ -0,0 +1,13 @@
+import { ThemeType } from "./types";
+
+export const lightMode: ThemeType = {
+  primary: "#D69E31",
+  secondary: "black",
+  tertiary: "white",
+};
+
+export const darkMode: ThemeType = {
+  primary: "#D69E31",
+  secondary: "white",
+  tertiary: "black",
+};
